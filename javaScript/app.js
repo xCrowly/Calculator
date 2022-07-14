95e918ceabeae5412d8fbcbef3111ef65f0380c2
xCrowly <ahhhhhmedeeeee@gmail.com>
2022-07-14 13:43:25 +0200

    finished the percentage function

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,42 +1,73 @@
 var theResult = document.getElementById("result");
 var theFormula = document.getElementById("userForm");
 var clickedButtonId;
+const operators = ['+', '-', '*', '/'];
 
-
+// to get the id of clicked button 
 function getInpId(clickedBut) {
     clickedButtonId = clickedBut;
-    console.log(clickedBut);
     showInput();
 }
 
+
 function showInput() {
-    if (theFormula.innerText == 0) {
+    if (theFormula.innerText == 0 & clickedButtonId != '=') {
         theFormula.innerHTML = clickedButtonId;
     } else {
-        if (theFormula.innerText.length <= 29) {
+        if (theFormula.innerText.length <= 29 & clickedButtonId != '=') {
             theFormula.innerHTML += clickedButtonId;
         }
     }
 }
 
+// to clear the user input
 function clearInput() {
     theFormula.innerHTML = 0;
 }
 
+// to clear the result
 function clearResult() {
     theResult.innerHTML = 0;
 }
 
+// to change the sign of the result
 function negative() {
     theResult.innerHTML *= -1;
 }
 
+// to devide the number after the last operator by 100
 function percentage() {
-    const tempForm = theFormula.innerText;
+    var tempForm = document.getElementById('userForm').innerHTML;
+    var foundOperator = false;
+    var numberToDivideBy100 = [];
     for (let i = tempForm.length; i > 0; i--) {
-        if (tempForm[i] in ["+", '-', '*', '/']) {
-            console.log(tempForm[i]); 
+        if (foundOperator == true) {
             break;
+        } else {
+            for (let j = 0; j < operators.length; j++) {
+                if (tempForm[i] == operators[j]) {
+                    console.log(tempForm[i], i);
+                    numberToDivideBy100 = tempForm.slice(i + 1, tempForm.length);
+                    console.log(numberToDivideBy100);
+                    tempForm = tempForm.slice(0, i + 1);
+                    foundOperator = true;
+                    break;
+                }
+            }
         }
     }
+
+    if (numberToDivideBy100.length != 0) {
+        if (numberToDivideBy100.length == 1 & numberToDivideBy100[0] == '.') {
+            console.log('Nan');
+        } else {
+            numberToDivideBy100 /= 100;
+            tempForm = tempForm + numberToDivideBy100;
+            document.getElementById('userForm').innerHTML = tempForm;
+        }
+    }
+}
+
+function showResult() {
+
 }
